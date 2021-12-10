e4df6a49c9b00d90af20a081d1b59c96b7ead0e6
Bilel MEDIMEGH <bilel.medimegh@gmail.com>
2021-12-10 19:21:19 +0100

    Support CORS

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,25 @@ mod services;
 #[macro_use]
 extern crate rocket;
 
+use rocket::http::Method;
+use rocket_cors::{AllowedOrigins, CorsOptions};
 use rocket_okapi::{mount_endpoints_and_merged_docs, openapi_get_routes};
 use rocket_okapi::rapidoc::{GeneralConfig, make_rapidoc, RapiDocConfig};
+
 use rocket_okapi::settings::{OpenApiSettings, UrlObject};
 
 #[rocket::main]
 async fn main() {
     let mut rocket = rocket::build();
-
+    let cors = CorsOptions::default()
+        .allowed_origins(AllowedOrigins::all())
+        .allowed_methods(
+            vec![Method::Get, Method::Post, Method::Patch]
+                .into_iter()
+                .map(From::from)
+                .collect(),
+        )
+        .allow_credentials(true);
     {
         let settings = OpenApiSettings::default();
         mount_endpoints_and_merged_docs! {
@@ -25,6 +36,7 @@ async fn main() {
     }
 
     rocket
+        .attach(cors.to_cors().unwrap())
         .mount("/ui/", make_rapidoc(&RapiDocConfig {
             general: GeneralConfig {
                 spec_urls: vec![UrlObject::new("Spec file", "/openapi.json")],
