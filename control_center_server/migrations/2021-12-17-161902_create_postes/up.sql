51b91f258ce427fa55dde0536fdafeee4a4749a4
Bilel MEDIMEGH <bilel.medimegh@gmail.com>
2021-12-18 18:02:20 +0100

    Ajout de postes par défaut dans le script

--- control_center_server/migrations/2021-12-17-161902_create_postes/up.sql
+++ control_center_server/migrations/2021-12-17-161902_create_postes/up.sql
@@ -12,3 +12,38 @@ create table postes
 create unique index postes_hostname_uindex
     on postes (hostname);
 
+insert into postes (is_on, is_compromised, os, hostname, ip) 
+VALUES (
+        true,
+        false,
+        'Debian',
+        'dinfo142',
+        '192.168.0.142'
+       );
+
+insert into postes (is_on, is_compromised, os, hostname, ip)
+VALUES (
+           false,
+           false,
+           'Windows',
+           'dinfo198',
+           '192.168.0.198'
+       );
+
+insert into postes (is_on, is_compromised, os, hostname, ip)
+VALUES (
+           true,
+           false,
+           'Windows',
+           'dinfo216',
+           '192.168.0.216'
+       );
+
+insert into postes (is_on, is_compromised, os, hostname, ip)
+VALUES (
+           true,
+           true,
+           'CentOS',
+           'dinfo234',
+           '192.168.0.234'
+       );
