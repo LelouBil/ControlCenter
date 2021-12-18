98665434ade8ad465ab8c8ce5f926abb27159068
Bilel MEDIMEGH <bilel.medimegh@gmail.com>
2021-12-18 21:28:11 +0100

    Filtre des posts fonctionnel

--- control_center_server/src/services/postes/data.rs
+++ control_center_server/src/services/postes/data.rs
@@ -1,6 +1,8 @@
-use serde::Serialize;
+use std::ops::{Range, RangeInclusive};
+use serde::{Serialize,Deserialize};
 use schemars::JsonSchema;
 use diesel::Queryable;
+use rocket::form::FromForm;
 
 #[derive(Serialize,JsonSchema,Queryable)]
 pub struct Poste{
@@ -9,4 +11,13 @@ pub struct Poste{
     pub os: String,
     pub hostname: String,
     pub ip: String,
+}
+
+#[derive(Deserialize,JsonSchema,FromForm)]
+pub struct PostesFilter{
+    pub is_on: Option<bool>,
+    pub is_compromised: Option<bool>,
+    pub os: Option<String>,
+    pub ip_range_start: Option<u64>,
+    pub ip_range_end: Option<u64>,
 }
--- control_center_server/src/services/postes/mod.rs
+++ control_center_server/src/services/postes/mod.rs
@@ -1,27 +1,66 @@
-use diesel::RunQueryDsl;
+use diesel::query_builder::{AsQuery, Query, SelectStatement};
+use diesel::{ExpressionMethods, QueryDsl, RunQueryDsl};
 use okapi::openapi3::OpenApi;
 use rocket::{Route};
+use rocket::futures::StreamExt;
 use rocket::serde::json::Json;
-use rocket_okapi::{openapi, openapi_get_routes_spec, };
+use rocket_okapi::{openapi, openapi_get_routes_spec};
+
 mod data;
 
 use data::Poste;
 use crate::database::DatabaseConnection;
 use crate::database::postes::dsl::postes;
+use crate::database::postes::{ip, is_on, os};
 
 use crate::services::authentication::LoggedInUser;
+use crate::services::postes::data::PostesFilter;
 
 
-pub fn routes() -> (Vec<Route>, OpenApi){
+pub fn routes() -> (Vec<Route>, OpenApi) {
     openapi_get_routes_spec![list_postes]
 }
 
 /// Liste les postes trouvés
 #[openapi(tag = "Postes")]
-#[get("/")]
-async fn list_postes(db : DatabaseConnection,_user : LoggedInUser) -> Json<Vec<Poste>>{
-    Json(db.run(|conn|{
-        postes.load::<Poste>(conn)
+#[get("/?<filtre..>")]
+async fn list_postes(db: DatabaseConnection, _user: LoggedInUser, filtre: PostesFilter) -> Json<Vec<Poste>> {
+    let mut postlist: Vec<Poste> = db.run(move |conn| {
+        let mut query = postes.as_query().into_boxed();
+
+        if let Some(value) = filtre.is_on {
+            query = query.filter(is_on.eq(value))
+        }
+
+        if let Some(value) = filtre.is_compromised {
+            query = query.filter(is_on.eq(value))
+        }
+
+        if let Some(value) = filtre.os {
+            query = query.filter(os.eq(value))
+        }
+        query.load::<Poste>(conn)
             .expect("Erreure de chargement des postes")
-    }).await)
+    }).await;
+
+    if let Some(value) = filtre.ip_range_start {
+        postlist.drain_filter(|poste| {
+            poste.ip.split(".").nth(3)
+                .map(|lastpart| {
+                    lastpart.parse::<u64>().ok()
+                })
+                .flatten()
+                .map_or(false, |num| value > num)
+        });
+    }
+    if let Some(value) = filtre.ip_range_end {
+        postlist.drain_filter(|poste| {
+            poste.ip.split(".").nth(3)
+                .map(|lastpart| lastpart.parse::<u64>().ok())
+                .flatten()
+                .map_or(false, |num| value < num)
+        });
+    }
+    
+    Json(postlist)
 }
