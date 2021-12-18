6989a88dc30fb1280d1dbcd9704164c42ca8081f
Bilel MEDIMEGH <bilel.medimegh@gmail.com>
2021-12-18 16:48:16 +0100

    User admin admin

--- control_center_server/migrations/2021-12-13-222928_create_users/up.sql
+++ control_center_server/migrations/2021-12-13-222928_create_users/up.sql
@@ -5,3 +5,5 @@ create table users
             primary key,
     password VARCHAR 
 );
+
+insert into users (username, password) VALUES ('admin','$argon2d$v=19$m=16,t=2,p=1$MTRxaldnbllJTms4U0xKYg$D0HxZsPvAHnc6bkOjfb2xA')
