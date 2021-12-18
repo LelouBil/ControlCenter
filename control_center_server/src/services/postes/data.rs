98babcd49a54a49ce18177146e1b06352c50fcc2
Bilel MEDIMEGH <bilel.medimegh@gmail.com>
2021-12-18 18:12:56 +0100

    Ajout de la liste des postes

--- control_center_server/src/services/postes/data.rs
+++ control_center_server/src/services/postes/data.rs
@@ -1,6 +1,8 @@
 use serde::Serialize;
 use schemars::JsonSchema;
-#[derive(Serialize,JsonSchema)]
+use diesel::Queryable;
+
+#[derive(Serialize,JsonSchema,Queryable)]
 pub struct Poste{
     pub is_on: bool,
     pub is_compromised: bool,
--- control_center_server/src/services/postes/mod.rs
+++ control_center_server/src/services/postes/mod.rs
@@ -1,3 +1,4 @@
+use diesel::RunQueryDsl;
 use okapi::openapi3::OpenApi;
 use rocket::{Route};
 use rocket::serde::json::Json;
@@ -5,6 +6,8 @@ use rocket_okapi::{openapi, openapi_get_routes_spec, };
 mod data;
 
 use data::Poste;
+use crate::database::DatabaseConnection;
+use crate::database::postes::dsl::postes;
 
 use crate::services::authentication::LoggedInUser;
 
@@ -16,41 +19,9 @@ pub fn routes() -> (Vec<Route>, OpenApi){
 /// Liste les postes trouvés
 #[openapi(tag = "Postes")]
 #[get("/")]
-async fn list_postes(_user : LoggedInUser) -> Json<Vec<Poste>>{
-    let mut random_postes: Vec<Poste> = Vec::new();
-    
-    let a = Poste{
-        ip: "192.168.0.142".to_string(),
-        os: "Debian".to_string(),
-        hostname: "dinfo142".to_string(),
-        is_compromised: false,
-        is_on: true
-    };
-    let b = Poste{
-        ip: "192.168.0.198".to_string(),
-        os: "Windows".to_string(),
-        hostname: "dinfo198".to_string(),
-        is_compromised: false,
-        is_on: false
-    };
-    let c = Poste{
-        ip: "192.168.0.216".to_string(),
-        os: "Windows".to_string(),
-        hostname: "dinfo216".to_string(),
-        is_compromised: false,
-        is_on: true
-    };
-    let d = Poste{
-        ip: "192.168.0.234".to_string(),
-        os: "CentOS".to_string(),
-        hostname: "dinfo234".to_string(),
-        is_compromised: true,
-        is_on: true
-    };
-    random_postes.push(a);
-    random_postes.push(b);
-    random_postes.push(c);
-    random_postes.push(d);
-
-    Json(random_postes)
+async fn list_postes(db : DatabaseConnection,_user : LoggedInUser) -> Json<Vec<Poste>>{
+    Json(db.run(|conn|{
+        postes.load::<Poste>(conn)
+            .expect("Erreure de chargement des postes")
+    }).await)
 }
